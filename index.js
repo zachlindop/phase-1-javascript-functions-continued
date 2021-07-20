8e7e643aa22aee43049320910fdedd31745e007a
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-07-20 16:42:22 -0700

    commit tree ae95cf96df46d0cbe30f2047eee32e3181327ac0

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(act = "go to the office"){
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective (flair = "*"){
+    return function innerFunction (str = "special"){
+        return `You are ${flair}${str}${flair}!`
+    } 
+}
+
+
+function outer(greeting, msg="It's a fine day to learn") {
+    const innerFunction =  function(name, lang="Python") {
+      return `${greeting}, ${name}! ${msg} ${lang}`;
+    }
+    return innerFunction;
+  } 
